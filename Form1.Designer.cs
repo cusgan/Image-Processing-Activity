4160e3d865987bba81a69fe0284cdf378ddde8ab
Wens <fmfgbascug@gmail.com>
2024-11-05 14:16:52 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Image_Processing_Activity
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            dipToolStripMenuItem = new ToolStripMenuItem();
+            openImageToolStripMenuItem = new ToolStripMenuItem();
+            saveImageToolStripMenuItem = new ToolStripMenuItem();
+            basicCopyToolStripMenuItem = new ToolStripMenuItem();
+            greyscaleToolStripMenuItem = new ToolStripMenuItem();
+            colorInversionToolStripMenuItem = new ToolStripMenuItem();
+            histogramToolStripMenuItem = new ToolStripMenuItem();
+            sepiaToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, dipToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openImageToolStripMenuItem, saveImageToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // dipToolStripMenuItem
+            // 
+            dipToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { basicCopyToolStripMenuItem, greyscaleToolStripMenuItem, colorInversionToolStripMenuItem, histogramToolStripMenuItem, sepiaToolStripMenuItem });
+            dipToolStripMenuItem.Name = "dipToolStripMenuItem";
+            dipToolStripMenuItem.Size = new Size(37, 20);
+            dipToolStripMenuItem.Text = "DIP";
+            dipToolStripMenuItem.Click += dipToolStripMenuItem_Click;
+            // 
+            // openImageToolStripMenuItem
+            // 
+            openImageToolStripMenuItem.Name = "openImageToolStripMenuItem";
+            openImageToolStripMenuItem.Size = new Size(180, 22);
+            openImageToolStripMenuItem.Text = "Open Image";
+            // 
+            // saveImageToolStripMenuItem
+            // 
+            saveImageToolStripMenuItem.Name = "saveImageToolStripMenuItem";
+            saveImageToolStripMenuItem.Size = new Size(180, 22);
+            saveImageToolStripMenuItem.Text = "Save Image";
+            // 
+            // basicCopyToolStripMenuItem
+            // 
+            basicCopyToolStripMenuItem.Name = "basicCopyToolStripMenuItem";
+            basicCopyToolStripMenuItem.Size = new Size(180, 22);
+            basicCopyToolStripMenuItem.Text = "Basic Copy";
+            // 
+            // greyscaleToolStripMenuItem
+            // 
+            greyscaleToolStripMenuItem.Name = "greyscaleToolStripMenuItem";
+            greyscaleToolStripMenuItem.Size = new Size(180, 22);
+            greyscaleToolStripMenuItem.Text = "Greyscale";
+            // 
+            // colorInversionToolStripMenuItem
+            // 
+            colorInversionToolStripMenuItem.Name = "colorInversionToolStripMenuItem";
+            colorInversionToolStripMenuItem.Size = new Size(180, 22);
+            colorInversionToolStripMenuItem.Text = "Color Inversion";
+            // 
+            // histogramToolStripMenuItem
+            // 
+            histogramToolStripMenuItem.Name = "histogramToolStripMenuItem";
+            histogramToolStripMenuItem.Size = new Size(180, 22);
+            histogramToolStripMenuItem.Text = "Histogram";
+            // 
+            // sepiaToolStripMenuItem
+            // 
+            sepiaToolStripMenuItem.Name = "sepiaToolStripMenuItem";
+            sepiaToolStripMenuItem.Size = new Size(180, 22);
+            sepiaToolStripMenuItem.Text = "Sepia";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openImageToolStripMenuItem;
+        private ToolStripMenuItem saveImageToolStripMenuItem;
+        private ToolStripMenuItem dipToolStripMenuItem;
+        private ToolStripMenuItem basicCopyToolStripMenuItem;
+        private ToolStripMenuItem greyscaleToolStripMenuItem;
+        private ToolStripMenuItem colorInversionToolStripMenuItem;
+        private ToolStripMenuItem histogramToolStripMenuItem;
+        private ToolStripMenuItem sepiaToolStripMenuItem;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace Image_Processing_Activity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dipToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
