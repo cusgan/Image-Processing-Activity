1b0a59c68a83d35f2de0ebec7382d97e5aa9fc2f
bambi <fmfgbascug@gmail.com>
2024-11-27 12:53:20 +0800

    green screen

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,8 @@ namespace Image_Processing_Activity
     public partial class Form1 : Form
     {
         Bitmap loaded, processed;
+        Bitmap bg, fg;
+        int mode = 0;
         public Form1()
         {
             InitializeComponent();
@@ -12,18 +14,33 @@ namespace Image_Processing_Activity
 
         private void dipToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void openImageToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            mode = 0;
             openFileDialog1.ShowDialog();
         }
 
         private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            loaded = new Bitmap(openFileDialog1.FileName);
-            pictureBox1.Image = loaded;
+            Bitmap opened_file = new Bitmap(openFileDialog1.FileName);
+            switch (mode)
+            {
+                case 0:
+                    loaded = opened_file;
+                    pictureBox1.Image = loaded;
+                    break;
+                case 1:
+                    bg = opened_file;
+                    pbBackground.Image = bg;
+                    break;
+                case 2: 
+                    fg = opened_file;
+                    pbForeground.Image = fg;
+                    break;
+            }
         }
 
         private void basicCopyToolStripMenuItem_Click(object sender, EventArgs e)
@@ -147,17 +164,46 @@ namespace Image_Processing_Activity
 
         private void btnLoadBG_Click(object sender, EventArgs e)
         {
-
+            mode = 1;
+            openFileDialog1.ShowDialog();
         }
 
         private void btnLoadFG_Click(object sender, EventArgs e)
         {
-
+            mode = 2;
+            openFileDialog1.ShowDialog();
         }
 
         private void btnSubtract_Click(object sender, EventArgs e)
         {
+            Color green = Color.FromArgb(0, 0, 255);
+            int threshold = 5;
 
+            int avg = (green.R + green.G + green.B) / 3;
+            processed = new Bitmap(fg.Width, fg.Height);
+
+            for (int x = 0 ; x < fg.Width; x++)
+            {
+                for (int y = 0 ; y < fg.Height; y++)
+                {
+                    Color fgpx = fg.GetPixel(x, y);
+                    Color bgpx = bg.GetPixel(x, y);
+
+                    int grey = (fgpx.R + fgpx.G +  fgpx.B) / 3;
+                    int subval = Math.Abs(grey - avg);
+
+                    if (subval < threshold)
+                    {
+                        processed.SetPixel(x, y, bgpx);                  
+                    }
+                    else
+                    {
+                        processed.SetPixel(x, y, fgpx);
+                    }
+                }
+            }
+
+            pbResult.Image = processed;
         }
     }
 }
